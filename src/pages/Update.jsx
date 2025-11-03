d43f7ff63176d358cf9a1af236f6d7070085e71a
skoegle8971 <admin@skoegle.com>
2025-11-03 14:46:43 +0530

    Update Update.jsx

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,5 +1,6 @@
 export default function Update() {
   const sheets = [
+     { month: "November", url: "https://sheet.zoho.in/sheet/open/8n0y25554ac7456f443608f0793ee1f95ad1a"},
     { month: "October", url: "https://sheet.zoho.in/sheet/open/2tv9c55d98e7511d14a68a84e6a2a4655ef86"},
     { month: "September", url: "https://sheet.zoho.in/sheet/open/tn41m36f24d8b615e41bc8525556d594d258f" },
     { month: "August", url: "https://sheet.zoho.in/sheet/open/n66b30448b72c4fff4b0da322eb2f7ac9418b" },
